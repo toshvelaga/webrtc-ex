6609b25b8407826dae3b587b1367e480bab1b6ed
Tosh Velaga <toshvelaga@gmail.com>
2022-03-15 22:49:14 -0600

    edit app.js and DailyRoom.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import Home from './Home'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import DailyRoom from './DailyRoom'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Router>
-          <Switch>
-            <Route path='/' exact component={Home} />
-            {/* <Route path='/:url' component={Video} /> */}
-            <Route path='/daily' component={DailyRoom} />
-          </Switch>
-        </Router>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          {/* <Route path='/:url' component={Video} /> */}
+          <Route path='/daily' component={DailyRoom} />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
--- src/DailyRoom.js
+++ src/DailyRoom.js
@@ -1,13 +1,26 @@
-import React from 'react'
-
-import { DailyProvider } from '@daily-co/daily-react-hooks'
 import DailyIframe from '@daily-co/daily-js'
+import { DailyProvider } from '@daily-co/daily-react-hooks'
+import React, { useState, useEffect } from 'react'
 
-function DailyRoom(props) {
-  // Create an instance of the Daily call object
-  const co = DailyIframe.createCallObject()
+const DAILY_URL = ''
 
-  return <DailyProvider callObject={co}>{props.children}</DailyProvider>
+export default function App() {
+  const [callObject, setCallObject] = useState(null)
+
+  useEffect(() => {
+    if (!DailyIframe) return
+
+    const newCallObject = DailyIframe.createCallObject()
+    setCallObject(newCallObject)
+    newCallObject.join({ url: DAILY_URL })
+  }, [])
+
+  // Pass the call object as a prop to the DailyProvider
+  // and render the app UI that needs access to
+  // Daily React Hooks as child components
+  return (
+    <DailyProvider callObject={callObject}>
+      <h1>hello</h1>
+    </DailyProvider>
+  )
 }
-
-export default DailyRoom
